76152495d0def6b82a2ccfac15ec0db44e619298
zewezz <chawit_poom@hotmail.com>
2025-11-14 17:05:16 +0700

    add sfx when mission complete

--- mission_point.gd
+++ mission_point.gd
@@ -1,3 +1,9 @@
 extends Node2D
+@onready var game_manager: Node = %GameManager
+@onready var mis_complete: AudioStreamPlayer2D = $mis_complete
 
 var card_inpoint = false
+
+func play_com():
+	mis_complete.play()
+	
--- scene/game_manager.gd
+++ scene/game_manager.gd
@@ -1,10 +1,16 @@
 extends Node
 
+
 var score = 0
 @onready var score_label: Label = $score_label
+@onready var mission_point: Node2D = $"../missionPoint"
+
 
 
 func add_point():
 	score += 1
 	print(score)
 	score_label.text = "Satisfied Customer: " + str(score) 
+	mission_point.play_com()
+	
+	
