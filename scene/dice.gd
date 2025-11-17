2cf0e62707e44a2f40c98384c5b0e46f0afb9d16
zewezz <chawit_poom@hotmail.com>
2025-11-17 21:32:57 +0700

    complete dice node in window

--- scene/dice.gd
+++ scene/dice.gd
@@ -5,30 +5,24 @@ var random_value = 4
 @onready var popup: Window = $Window
 @onready var dice_roll: AnimatedSprite2D = $Window/Dice_roll
 
-func dicey():
+
+func _ready():
 	randomize()
-	random_value = randi_range(1, 6)
-	print(random_value)
+	popup.hide()
+
+func set_dice_result():
+	random_value = randi_range(1, 4)
+	print("Final Dice Roll: ", random_value)
+	dice_roll.play(str(random_value))
 
 func _on_window_close_request():
 	popup.hide()
 	
 func _input(event):
-	
-	if event.is_action_pressed("Roll"):
+	if event.is_action_pressed("Roll") and not dice_roll.is_playing():
 		popup.show()
-		dicey()
-		
-func _process(delta):
-	match random_value:
-		1:
-			dice_roll.play("1")
-		2:
-			dice_roll.play("2")
-		3:
-			dice_roll.play("3")
-		4:
-			dice_roll.play("4")
-	
-	
-	
+		dice_roll.play("wait_dice")
+		await get_tree().create_timer(1.0).timeout
+		set_dice_result()
+		await get_tree().create_timer(1.0).timeout
+		popup.hide()
