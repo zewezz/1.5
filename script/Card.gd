c23b95d639f25102a6a9a60c889f7511cdade5e0
DESKTOP-0KJNQDJ\USER <narin14175@gmail.com>
2026-01-07 22:37:35 +0700

    feat: add stat screen to all card

--- script/Card.gd
+++ script/Card.gd
@@ -3,11 +3,14 @@ extends Node2D
 signal hovered
 signal hovered_off
 
+@export
+var stat_screen: Node2D
+
 var inhand_pos: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# all cards must be the child of the cardmanager!!!
-	get_parent().connect_card_signal(self)
+	connect_hovering_signal_with_parent()
 	#$"TextureProgressBar".visible = false
 
 
@@ -15,12 +18,23 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func connect_hovering_signal_with_parent() -> void:
+	get_parent().connect_card_signal(self)
+
+func show_stat_screen() -> void:
+	stat_screen.visible = true
+	
+func hide_stat_screen() -> void:
+	stat_screen.visible = false
+
 func _on_area_2d_mouse_entered() -> void:
 	emit_signal("hovered", self)
+	show_stat_screen()
 
 
 func _on_area_2d_mouse_exited() -> void:
 	emit_signal("hovered_off", self)
+	hide_stat_screen()
 
 
 	
